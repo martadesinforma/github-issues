a3aaad2454921c0528b56e4b29ea8de26c951720
Marta Ocana <martacyta2021@gmail.com>
2024-11-07 11:45:28 +0100

    create get-labels function

---
+++ src/app/modules/issues/actions/get-labels.ts
@@ -0,0 +1,22 @@
+import { sleep } from "../../../helpers/sleep";
+import { GitHubLabel } from "../interfaces/github-label.interface";
+
+
+//esta función la vamos a llamar en el servicio issues.service.ts
+export const getLabels = async (): Promise<GitHubLabel[]> => {
+
+  await sleep(1500); //llamamamos a la funcion sleep() del archivo sleep.ts
+
+  try {
+    const resp = await fetch(`http://api.github.com/repos/angular/angular/labels`);//resp es un objeto Response que luce asi:  Response {type: 'cors', url: 'https://api.github.com/repos/angular/angular/labels', redirected: true, status: 200, ok: true, …}
+
+    if (!resp.ok) throw "Can't load labels";
+
+    const labels: GitHubLabel[] = await resp.json(); // El .json() obtiene el body de la response pero traducido de formato JSON a formato JavaScript.Me regresa en este caso un arreglo de GitHubLabel. labels luce de esta forma: Array(30) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}...]
+
+    return labels;
+  } catch (error) {
+    throw "Can't load labels";
+  }
+};
+
