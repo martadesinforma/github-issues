551e26f9a6cf97fae866f138601a933c315bb9a2
Marta Ocana <martacyta2021@gmail.com>
2024-11-07 11:46:39 +0100

    update issues-list-page component

--- src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -1,12 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IssuesService } from '../../services/issues.service';
+import { LabelsSelectorComponent } from '../../components/labels-selector/labels-selector.component';
 
 @Component({
   selector: 'app-issues-list-page',
   standalone: true,
   imports: [
-    CommonModule,
+    CommonModule, RouterLink, LabelsSelectorComponent,
   ],
   templateUrl: './issues-list-page.component.html',
 })
-export default class IssuesListPageComponent { }
+export default class IssuesListPageComponent {
+
+  public issuesService = inject(IssuesService);
+
+  get labelsQuery() { //para que en el html llamemos a este get que hace referencia directamente a abelsQuery del servicio  issuesService
+    return this.issuesService.labelsQuery;
+  }
+}
