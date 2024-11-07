53d3d5dc992a6ce185759fbe6ea8b6070dd5927a
Marta Ocana <martacyta2021@gmail.com>
2024-11-07 11:49:36 +0100

    incorporate TanStack Query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AngularQueryDevtools } from '@tanstack/angular-query-devtools-experimental';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AngularQueryDevtools],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZ
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAngularQuery, QueryClient } from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
   providers: [
    /*  provideZoneChangeDetection({ eventCoalescing: true }), */
     provideRouter(routes),
     provideExperimentalZonelessChangeDetection(),
+    provideAngularQuery(new QueryClient()),
   ]
 };
