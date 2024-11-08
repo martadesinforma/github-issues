912b57c1b3e1665f64c70ad4072ac91161179087
Marta Ocana <martacyta2021@gmail.com>
2024-11-08 14:31:01 +0100

    update issue service

--- src/app/modules/issues/services/issue.service.ts
+++ src/app/modules/issues/services/issue.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, signal } from '@angular/core';
 import { injectQuery } from '@tanstack/angular-query-experimental';
 import { getIssueByNumber } from '../actions/get-issue-by-number.action';
+import { getIssueCommentsByNumber } from '../actions/get-issue-comments-by-number.action';
 
 @Injectable({
   providedIn: 'root'
@@ -17,11 +18,11 @@ export class IssueService {
     enabled: this.issueNumber() !== null, //enabled evaluará si this.issueNumber() no es null. Si this.issueNumber() tiene un valor (por ejemplo, "123"), entonces enabled será true, y la consulta queryFn se disparará, ejecutando getIssueByNumber con el valor actual de this.issueNumber(). Si this.issueNumber() es null, enabled será false, lo que evita que la consulta se ejecute.
   }));
 
-  /* issueCommentsQuery = injectQuery(() => ({
+  issueCommentsQuery = injectQuery(() => ({
     queryKey: ['issue', this.issueNumber(), 'comments'],
     queryFn: () => getIssueCommentsByNumber(this.issueNumber()!),
     enabled: this.issueNumber() !== null,
-  })); */
+  }));
 
   setIssueNumber(issueId: string) { //El valor de issueId es proporcionado por el componente issue-page. issueId va a tener  el valor del parámetro number de la URL de la ruta
     this.issueNumber.set(issueId);
