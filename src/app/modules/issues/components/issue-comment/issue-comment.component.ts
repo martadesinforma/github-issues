85068addd74e4df479c06e83022fe82d462e4dd4
Marta Ocana <martacyta2021@gmail.com>
2024-11-08 14:30:19 +0100

    update issue-comment component

--- src/app/modules/issues/components/issue-comment/issue-comment.component.ts
+++ src/app/modules/issues/components/issue-comment/issue-comment.component.ts
@@ -11,7 +11,7 @@ import { MarkdownModule} from 'ngx-markdown';
   ],
   templateUrl: './issue-comment.component.html',
 })
-export class IssueCommentComponent {//El componente IssueCommentComponent se va a insertar en el archivo issue-page.component.html mediante <issue-comment [issue]="issueQuery.data()!"></issue-comment>
+export class IssueCommentComponent {//El componente IssueCommentComponent se va a insertar en el archivo issue-page.component.html mediante <issue-comment [issue]="issueQuery.data()!"></issue-comment> y mediante <issue-comment [issue]="comment"></issue-comment>. De esta forma  maneja diferentes datos para representar tanto el "issue" principal como sus comentarios asociados.
 
   issue = input.required<GitHubIssue>(); //Esta es información proveniente del componente issue-page
 
