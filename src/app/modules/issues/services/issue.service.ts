297f018af0a0a8656dbca53bb9755ec9c1423577
Marta Ocana <martacyta2021@gmail.com>
2024-11-18 11:01:55 +0100

    update services

--- src/app/modules/issues/services/issue.service.ts
+++ src/app/modules/issues/services/issue.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal } from '@angular/core';
-import { injectQuery } from '@tanstack/angular-query-experimental';
+import { injectQuery, injectQueryClient } from '@tanstack/angular-query-experimental';
 import { getIssueByNumber } from '../actions/get-issue-by-number.action';
 import { getIssueCommentsByNumber } from '../actions/get-issue-comments-by-number.action';
 
@@ -10,7 +10,9 @@ import { getIssueCommentsByNumber } from '../actions/get-issue-comments-by-numbe
 // Este servicio se va a utilizar en el componente issue-page
 export class IssueService {
 
+  private queryClient = injectQueryClient(); //QueryClient es una instancia que maneja la lógica principal de TanStack Query. Permite ejecutar consultas, prefetch, cachear datos y controlar el estado de las consultas a nivel global en la aplicación.
   private issueNumber = signal<string | null>(null);
+
 
   issueQuery = injectQuery(() => ({ //issueQuery es un  objeto que almacena propiedades y métodos relevantes
     queryKey: ['issue', this.issueNumber()],
@@ -26,6 +28,15 @@ export class IssueService {
 
   setIssueNumber(issueId: string) { //El valor de issueId es proporcionado por el componente issue-page. issueId va a tener  el valor del parámetro number de la URL de la ruta
     this.issueNumber.set(issueId);
+  }
+
+  //este método lo vamos a llamar en el issue-item component
+  prefetchIssue(issueId: string) { // carga y almacena en caché los datos de un issue con el ID issueId
+    this.queryClient.prefetchQuery({
+      queryKey: ['issue', issueId],
+      queryFn: () => getIssueByNumber(issueId), // recuperará los datos específicos del issue con el número issueId.
+      staleTime: 1000 * 60 * 5, //Define el tiempo de "vigencia" de los datos en la caché (5 minutos aquí), para no hacer llamadas innecesarias si el usuario vuelve a ver el mismo issue dentro de ese tiempo.
+    });
   }
 
 }
--- src/app/modules/issues/services/issues.service.ts
+++ src/app/modules/issues/services/issues.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { injectQuery } from '@tanstack/angular-query-experimental';
 import { getLabels } from '../actions/get-labels.action';
 import { getIssues } from '../actions/get-issues.action';
+import { State } from '../interfaces/github-issue.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,43 @@ import { getIssues } from '../actions/get-issues.action';
 //Este código en Angular define un servicio (IssuesService) que utiliza TanStack Query para realizar una solicitud de datos mediante la función getLabels y getIssues. Cuando labelsQuery se activa (por ejemplo, cuando el componente  issues-list-page lo utiliza), TanStack Query comprueba en su caché si hay datos almacenados bajo las claves ['labels'] y ['issues']. Si no hay datos en la caché o estos han caducado, TanStack Query ejecuta getLabels() y getIssues() para obtener datos de la API de GitHub. Una vez que los datos son recibidos, TanStack Query los almacena en la caché, bajo las claves ['labels'] y  ['issues'], y los proporciona al componente que utiliza labelsQuery.
 export class IssuesService {
 
+  selectedState = signal<State>(State.All); // Va a recibir como valor un 'all', 'open' o 'closed'.
+ /*  selectedLabels = signal(new Set<string>()); */ //significa que selectedLabels almacenará un conjunto de etiquetas (strings). En este caso, se le pasa un Set<string> vacío (es decir, new Set<string>()) como valor inicial. Esto significa que selectedLabels comienza vacío y, a medida que se añaden o eliminan etiquetas, el estado reactivo se actualizará automáticamente.
+
+
   labelsQuery = injectQuery(() => ({ //labelsQuery es un observable que representa el estado de la consulta de etiquetas y se puede suscribir para obtener los datos
     queryKey: ['labels'], //queryKey: Define la clave única ['labels'] para esta consulta. Esto identifica la consulta en la caché de TanStack Query, permitiendo almacenar y reutilizar los resultados.
     queryFn: () => getLabels(), //queryFn: Es la función que se ejecuta para obtener los datos. Aquí, es una llamada directa a getLabels(), que realiza la solicitud HTTP.
   }));
 
   issuesQuery = injectQuery(() => ({
-    queryKey: ['issues'],
-    queryFn: () => getIssues(),
+    queryKey: ['issues',
+      {
+        state: this.selectedState(),
+       /*  selectedLabels: [...this.selectedLabels()] */ //El operador de propagación (...) toma todos los elementos del Set dentro de selectedLabels()  y los coloca en un nuevo arreglo ([]). Trabajamos con una copia. Si agregamos o eliminamos elementos en este array, no afectarás la señal original selectedLabels
+      }
+    ],
+    queryFn: () => getIssues(this.selectedState(), /* [...this.selectedLabels()] */),
   }));
+
+
+  //este método lo vamos a llamar en el  issues-list-page.component. Va a recibir como state un 'all', 'open' o 'closed'.
+  showIssuesByState(state: State) {
+    this.selectedState.set(state);
+  }
+
+
+  //Este método sirve para agregar o quitar una etiqueta de la colección selectedLabels, dependiendo de si ya está presente o no en el conjunto (Set). Vamos a llamar este método en el componente labels-selector y va a recibir como argumento el nombre de uno de los labels
+/*   toggleLabel(label:string){
+    const labels = this.selectedLabels(); //Obtiene el conjunto actual de etiquetas seleccionadas.
+
+    if(labels.has(label)) {  //Si el conjunto ya contiene la etiqueta:
+      labels.delete(label); //Elimina la etiqueta del conjunto
+    }else{ //Si el conjunto no contiene la etiqueta
+      labels.add(label) //Añade la etiqueta al conjunto
+    }
+
+    this.selectedLabels.set(new Set(labels)); //para actualizar la señal selectedLabels
+  } */
 
 }
