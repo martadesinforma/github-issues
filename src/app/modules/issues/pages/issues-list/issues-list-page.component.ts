8b9bdfd7687c78abaee972fdd6e94905a761abac
Marta Ocana <martacyta2021@gmail.com>
2024-11-18 11:02:23 +0100

    update issues-list-page component

--- src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -4,6 +4,7 @@ import { RouterLink } from '@angular/router';
 import { IssuesService } from '../../services/issues.service';
 import { LabelsSelectorComponent } from '../../components/labels-selector/labels-selector.component';
 import { IssueItemComponent } from "../../components/issue-item/issue-item.component";
+import { State } from '../../interfaces/github-issue.interface';
 
 @Component({
   selector: 'app-issues-list-page',
@@ -11,7 +12,7 @@ import { IssueItemComponent } from "../../components/issue-item/issue-item.compo
   imports: [
     CommonModule, LabelsSelectorComponent,
     IssueItemComponent
-],
+  ],
   templateUrl: './issues-list-page.component.html',
 })
 export default class IssuesListPageComponent {
@@ -24,5 +25,28 @@ export default class IssuesListPageComponent {
 
   get issuesQuery() { //para que en el html llamemos a este get que hace referencia directamente a issuesQuery del servicio  issuesService
     return this.issuesService.issuesQuery;
+  }
+
+
+  onChangeState(newState: string) { //lo llamamos en el issues-list-page.html cada vez que clicamos el boton 'all', 'open' o 'closed' y este mñetodo recibe como newState un 'all', 'open' o 'closed'.
+    let state;
+
+    switch (newState) {
+      case 'all':
+        state = State.All;
+        break;
+      case 'open':
+        state = State.Open;
+        break;
+      case 'closed':
+        state = State.Closed;
+        break;
+      default:
+        state = State.All; // Valor por defecto si newState no coincide con ningún caso
+        break;
+    }
+
+    this.issuesService.showIssuesByState(state);
+
   }
 }
