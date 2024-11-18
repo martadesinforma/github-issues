3353e002dad56e533642e7ceee84d20a975454ab
Marta Ocana <martacyta2021@gmail.com>
2024-11-18 11:03:31 +0100

    update issue-item component

--- src/app/modules/issues/components/issue-item/issue-item.component.ts
+++ src/app/modules/issues/components/issue-item/issue-item.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { GitHubIssue, State } from '../../interfaces/github-issue.interface';
+import { IssueService } from '../../services/issue.service';
 
 @Component({
   selector: 'issue-item',
@@ -14,9 +15,14 @@ import { GitHubIssue, State } from '../../interfaces/github-issue.interface';
 export class IssueItemComponent { //El componente IssueItemComponent se va a insertar en el archivo issues-list-page.component.html mediante <issue-item></issue-item>
 
   issue = input.required<GitHubIssue>(); //Esta es información proveniente del componente issues-list-page
+  issueService = inject(IssueService);
 
   get isOpen() {
     return this.issue().state === State.Open;
+  }
+
+  prefetcData() { //prepara los datos de un issue específico de la lista para que estén listos en la caché si el usuario decide acceder a él.
+    this.issueService.prefetchIssue(this.issue().number.toString());
   }
 
 }
