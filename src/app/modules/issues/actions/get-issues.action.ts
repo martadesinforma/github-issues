040825c1a2e42dbb5d0285f7cf4505ca4a5d1af8
Marta Ocana <martacyta2021@gmail.com>
2024-11-18 11:04:07 +0100

    update get-issues action (function)

--- src/app/modules/issues/actions/get-issues.action.ts
+++ src/app/modules/issues/actions/get-issues.action.ts
@@ -1,6 +1,6 @@
 import { environment } from "../../../../environments/environment.development";
 import { sleep } from "../../../helpers/sleep";
-import { GitHubIssue } from "../interfaces/github-issue.interface";
+import { GitHubIssue, State } from "../interfaces/github-issue.interface";
 
 
 
@@ -8,18 +8,25 @@ const BASE_URL = environment.baseUrl;
 
 
 //esta función la vamos a llamar en el servicio issues.service.ts
-export const getIssues = async (): Promise<GitHubIssue[]> => {
+export const getIssues = async (state: State = State.All, /* selectedLabels:string[] */): Promise<GitHubIssue[]> => { //en caso de no recibir argumento, por defecto será 'all'
 
   await sleep(1500); //llamamamos a la funcion sleep() del archivo sleep.ts
 
+  const params = new URLSearchParams(); //params es una instancia de URLSearchParams, una clase que permite crear y manipular una cadena de parámetros de URL.
+  params.append('state', state); //se agrega un parámetro de estado (state) a params utilizando el método append. Esto crea la cadena state=valor en los parámetros de la URL, donde valor será el estado especificado (all, open, o closed).
+
+  /* if(selectedLabels.length > 0 ){
+    params.append('labels', selectedLabels.join(','));
+  } */
+
   try {
     const resp = await fetch( //resp es un objeto Response que luce asi: Response {type: 'cors', url: 'https://api.github.com/repos/angular/angular/issues', redirected: true, status: 200, ok: true, …}
-      `${BASE_URL}/issues`,
+      `${BASE_URL}/issues?${params}`,
     );
 
     if (!resp.ok) throw "Can't load issues";
 
-    const issues: GitHubIssue[] = await resp.json(); // El .json() obtiene el body de la response pero traducido de formato JSON a formato JavaScript .Me regresa en este caso un arreglo de GitHubIssue. labels luce de esta forma: Array(30) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}...]
+    const issues: GitHubIssue[] = await resp.json(); // El .json() obtiene el body de la response pero traducido de formato JSON a formato JavaScript .Me regresa en este caso un arreglo de GitHubIssue. issues luce de esta forma: Array(30) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}...]
 
 
 
